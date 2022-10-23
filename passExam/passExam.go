567cde46626762da5959192439de4f596a3d98a3
KosmanG <115623146+KosmanG@users.noreply.github.com>
2022-10-23 08:54:25 +0600

    Add files via upload

---
+++ passExam/passExam.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Enter your grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if grade == 100 {
+		fmt.Println("Perfect!")
+	} else if grade >= 60 {
+		fmt.Println("You pass.")
+	} else {
+		fmt.Println("You fail!")
+	}
+}
